8d6fa495159c1e15861adf7c6ee646275a40d378
Magali-Chen Goulet <magali.goulet@hotmail.com>
2021-05-19 14:54:04 -0400

    fix to run without derivative and output my and Sy

--- R/regression.R
+++ R/regression.R
@@ -26,6 +26,7 @@ regression <- function(NN, x, y, trainIdx, testIdx){
   # Train net
   NN$trainMode = 1
   NN$batchSize = NN$batchSizeList[1]
+  NN <- initialization_net(NN)
   NN <- parameters(NN)
   NN <- covariance(NN)
 
@@ -122,6 +123,6 @@ regression <- function(NN, x, y, trainIdx, testIdx){
     cat(sprintf("Results for Run # %s, RMSE: %s and LL: %s", s, RMSElist[s], LLlist[s]))
   }
   metric = list("RMSElist" = RMSElist, "LLlist" = LLlist)
-  outputs <- list(mp, Sp, metric, trainTimelist)
+  outputs <- list(mp, Sp, metric, trainTimelist, ynTest, SynTest)
   return(outputs)
 }
